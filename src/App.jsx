9243b5ff6162b15e66bfe53416d35d6b2c4ab85c
itz_Malc0m <danieldimfwina@gmail.com>
2024-04-08 11:47:42 +0100

    create pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Projects from './pages/Projects'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
 
 export default function M4lc0m() {
   return (
-    <div className='text-3xl text-red-600'>M4lc0m</div>
+   <BrowserRouter>
+   <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/about' element={<About />} />
+    <Route path='/projects' element={<Projects />} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/sign-in' element={<SignIn />} />
+    <Route path='/sign-up' element={<SignUp />} />
+   </Routes>
+   </BrowserRouter>
   )
 }
